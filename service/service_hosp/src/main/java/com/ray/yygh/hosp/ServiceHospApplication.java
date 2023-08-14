4730a1378062dd3dd8dfd45464affb8036e28b3c
MondRay_Huang <2469245827@qq.com>
2023-08-14 20:40:02 +0800

    8.14 增加了 Swagger2测试，带条件分页查询医院设置

--- service/service_hosp/src/main/java/com/ray/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/ray/yygh/hosp/ServiceHospApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class,args);
-        //"id"	"hosname"	"hoscode"	"api_url"	"sign_key"	"contacts_name"	"contacts_phone"	"status"	"create_time"	"update_time"	"is_deleted"
-        //"6"	"北京协和医院"	"1000_0"	"http://localhost:9998"	"674c4139707728439a6510eae12deea9"	"22"	"22"	"1"	"9/12/2020 09:33:46"	"12/12/2020 14:28:35"	"0"
     }
 
 }
--- service/service_hosp/src/main/java/com/ray/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/ray/yygh/hosp/controller/HospitalSetController.java
@@ -1,15 +1,22 @@
 package com.ray.yygh.hosp.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ray.yygh.common.result.Result;
 import com.ray.yygh.hosp.service.HospitalSetService;
 import com.ray.yygh.model.hosp.HospitalSet;
+import com.ray.yygh.vo.hosp.HospitalSetQueryVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -17,10 +24,45 @@ public class HospitalSetController {
     private HospitalSetService hospitalSetService;
 
     //1.查询医院设置表所有信息
+    @ApiOperation("查询所有hosipitalSet")
     @GetMapping("findAll")
-    public List<HospitalSet> findAll(){
+    public Result findAll(){
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok(list);
+    }
+
+    @ApiOperation("逻辑删除hospitalSet")
+    @DeleteMapping("{id}")
+    public Result removeHospitalSet(@PathVariable Long id){
+        boolean flag = hospitalSetService.removeById(id);
+        if(flag){
+            return Result.ok();
+        }else{
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("条件分页查询")
+    @GetMapping("findPageHospSet/{current}/{limit}")
+    public Result findPageHospSet(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  HospitalSetQueryVo hospitalSetQueryVo){
+
+
+    //创建分页对象，传递当前页
+        Page<HospitalSet> page = new Page<>(current,limit);
+        System.out.println(limit);
+        QueryWrapper<HospitalSet> hospitalSetQueryWrapper = new QueryWrapper<>();
+        String hosname = hospitalSetQueryVo.getHosname();
+        String hoscode = hospitalSetQueryVo.getHoscode();
+        if(!StringUtils.isEmpty(hosname)){
+            hospitalSetQueryWrapper.like("hosname",hosname);
+        }
+        if(!StringUtils.isEmpty(hoscode)){
+            hospitalSetQueryWrapper.eq("hoscode",hoscode);
+        }
+        Page<HospitalSet> hospitalSetPage = hospitalSetService.page(page,hospitalSetQueryWrapper);
+        return Result.ok(hospitalSetPage);
     }
 
 }
