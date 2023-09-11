9ff92fd022622de9a3c43643acee27008f27fa1e
MondRayHuang <2469245827@qq.com>
2023-09-11 22:39:08 +0800

    9.10 搭好了win端的redis和nginx

--- FrontEnd/hospital_front_end/config/dev.env.js
+++ FrontEnd/hospital_front_end/config/dev.env.js
@@ -5,6 +5,12 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // BASE_API: '"http://localhost:8202"',
+
   // mac版本
-  BASE_API: '"http://10.211.55.4:8000"',
+  // BASE_API: '"http://10.211.55.4:8000"',
+  
+  // win版本
+  BASE_API: '"http://192.168.140.128:8000"',
+
 })
