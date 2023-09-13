33b8ccc5bc8c88229aff4af966e96cc303c10d4f
MondRayHuang <2469245827@qq.com>
2023-09-14 01:29:38 +0800

    9.10 搭好了win端的redis和nginx 9.11 加入了医院管理的签名校验

---
+++ hospital-manage/src/main/java/com/ray/hospital/mapper/OrderInfoMapper.java
@@ -0,0 +1,10 @@
+package com.ray.hospital.mapper;
+
+import com.ray.hospital.model.OrderInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
+
+}
---
+++ hospital-manage/src/main/java/com/ray/hospital/mapper/ScheduleMapper.java
@@ -0,0 +1,10 @@
+package com.ray.hospital.mapper;
+
+import com.ray.hospital.model.Schedule;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+
+}
